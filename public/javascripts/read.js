e2c97b94a9352ef0cf89db83913ca34b78cb8d0c
Nate Taylor <taylonr@gmail.com>
2020-07-25 12:14:54 -0500

    Writing the code for read

--- public/javascripts/read.js
+++ public/javascripts/read.js
@@ -1,1 +1,23 @@
-(function () {})();
+document.getElementsByTagName("button")[0].onclick = function () {
+  const results = document.getElementById("results");
+  const value = document.getElementById("product-id").value;
+
+  axios.get(`/api/products/${value}`).then(updateTemplate);
+};
+
+function updateTemplate({ data }) {
+  const template = document.querySelector("#result");
+  const currentProduct = document.getElementById("product-result");
+  if (currentProduct) {
+    currentProduct.remove();
+  }
+
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("#title").textContent = data.name;
+  clone.querySelector("#price").textContent = data.price;
+  clone.querySelector("#quantity").textContent = data.quantity;
+  clone.querySelector("#color").textContent = data.color;
+  clone.querySelector("#description").textContent = data.description;
+
+  results.appendChild(clone);
+}
--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
+  router.get("/products/:id", (req, res) => {
+    res.send(db.get("products").find({ id: req.params.id }).value());
+  });
+
+  router.post("/products", (req, res) => {
+    res.send(db.get("products").insert(req.body).write());
+  });
+
   return router;
 };
